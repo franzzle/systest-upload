766ce9c856ca4a16a8bbdfc02061f559553d68c2
Frans Kramer <me@franzzle.com>
2016-09-10 17:05:30 +0200

    added more fields and styling

--- client/main.js
+++ client/main.js
@@ -1,18 +1,27 @@
 angular.module('fileUpload', ['ngFileUpload'])
-    .controller('MyCtrl', ['Upload', '$scope', function (Upload, $scope) {
+    .controller('MyCtrl', ['Upload', '$scope','$timeout', function (Upload, $scope, $timeout) {
         $scope.upload = function (file) {
             file.upload = Upload.upload({
                 url: 'http://localhost:8877/upload', //webAPI exposed to upload the file
-                data: { file: file, nameTest: $scope.nameTest } //pass file as data, should be user ng-model
+                data: { file: file, test: { 
+                    nameTest:$scope.nameTest,
+                    description:$scope.description,
+                    expectedModality:$scope.expectedModality} 
+                } //pass file as data, should be user ng-model
             });
 
             //upload function returns a promise
             file.upload.then(function (response) {
                 //validate success
                 if (response.data.error_code === 0) {
-                    console.log('Success ' + response.config.data.file.name + 'uploaded. responseonse: ');
+                    $scope.succesMsg = 'Success uploading' + response.config.data.file.name;
+                    $scope.showMessage = true;
+                    $timeout(function() {
+                        $scope.showMessage = false;
+                    }, 3000);
+
                 } else {
-                    console.log('An error occured');
+                    $scope.errorMsg = response.status + ': ' + response.data;
                 }
             }, function (response) {
                 console.log('Error status: ' + response.status);
